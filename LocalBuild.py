6d4655d5e1afa223c67681a518e4e4878eb939ea
Andrew Wood <aew61@case.edu>
2016-07-25 14:31:50 -0400

    download and check build scripts versions

--- LocalBuild.py
+++ LocalBuild.py
@@ -1,6 +1,7 @@
 
 # SYSTEM IMPORTS
 import os
+import pymongo
 import sys
 
 
@@ -17,11 +18,10 @@ def urljoin(url, *urls):
 
 
 def downloadBuildScripts():
-    import pymongo
     import requests
     import tarfile
 
-    downloadedBuildScriptsPath = os.path.join(buildScriptsDir, "Scripts_download")
+    downloadedBuildScriptsPath = os.path.join(buildScriptsDir, "downloads")
     if not os.path.exists(downloadedBuildScriptsPath):
         os.makedirs(downloadedBuildScriptsPath)
     client = pymongo.MongoClient(os.environ["MONGODB_URI"])
@@ -33,18 +33,54 @@ def downloadBuildScripts():
         }
     ).sort("build_num")][-1]
     filePath = os.path.join(downloadedBuildScriptsPath,
-                            mostRecentBuildScriptsRecord["filename"] + mostRecentBuildScriptsRecord["filetype"])
-    response = requests.get(urljoin(os.environ["FILESERVER_URI"], "BuildScripts/"), stream=True)
+                            mostRecentBuildScriptsRecord["fileName"] + mostRecentBuildScriptsRecord["filetype"])
+    response = requests.get(urljoin(os.environ["FILESERVER_URI"], "BuildScripts/",
+                                    mostRecentBuildScriptsRecord["fileName"] + mostRecentBuildScriptsRecord["filetype"]),
+                            stream=True)
+
+    numBytes = len(response.content)
+    currentBytes = 0.0
+    minPercentToPrint = 0
+    print("Starting download (%s bytes):" % numBytes)
     with open(filePath, "wb") as f:
-        for chunk in response.iter_content():
+        for chunk in response.iter_content(numBytes / 10):
+            if currentBytes/numBytes >= minPercentToPrint:
+                print("[%s%%]" % int(currentBytes/numBytes * 100)),
+                minPercentToPrint += 0.1
             f.write(chunk)
+            currentBytes += len(chunk)
+    print("Download done")
     with tarfile.open(filePath, "r:gz") as tarFile:
         # extract out scripts and config dirs and move it to currentDir/build
-        tarFile.extractAll(path=os.path.join(currentDir, "build"), members=["scripts", "config"])
+        tarFile.extractall(path=os.path.join(currentDir, "build"),
+                           members=[x for x in tarFile.getmembers() if "LICENSE" not in x.name])
 
 
-if not os.path.exists(buildScriptsDir):
-    os.makedirs(buildScriptsDir)
+def updateBuildScripts():
+    if not os.path.exists(buildScriptsDir):
+        os.makedirs(buildScriptsDir)
+        return True
+    # if this method is called, can safely assume build/scripts/ exists
+    client = pymongo.MongoClient(os.environ["MONGODB_URI"])
+    db = client["BuildScripts"]
+    coll = db["src"]
+    mostRecentBuildScriptsRecord = [x for x in coll.find(
+        {
+            "config": "src"
+        }
+    ).sort("build_num")][-1]
+    mostRecentBuildNum = [mostRecentBuildScriptsRecord["major_version"],
+                          mostRecentBuildScriptsRecord["minor_version"],
+                          mostRecentBuildScriptsRecord["patch"],
+                          mostRecentBuildScriptsRecord["build_num"]]
+    print("Most recent BuildScripts version: %s" % mostRecentBuildNum)
+    currentFile = max(os.listdir(os.path.join(buildScriptsDir, "downloads")))
+    currentBuild = [int(x) for x in currentFile.split("_")[1].split(".")]
+    print("Currently have BuildScripts version: %s" % currentBuild)
+    return mostRecentBuildNum > currentBuild
+    
+
+if updateBuildScripts():
     downloadBuildScripts()
 sys.path.extend([currentDir, buildScriptsDir])  # now we can import modules from <currentDirectory>/scripts
 
@@ -63,61 +99,67 @@ class LocalBuild(ProjectBuild.ProjectBuild):
                              self.package,
                              self.uploadPackagedVersion]
 
-    def generateConfig(self, asyncConfigPath=None, asyncConfigFileName=None):
-        outIncludeDir = os.path.join(FileSystem.getDirectory(FileSystem.OUT_ROOT),
-                                     "include")
-        projectLogDir = FileSystem.getDirectory(FileSystem.LOG_DIR, self._config, self._project_name)
-        asyncConfig = None
-        if asyncConfigPath is None:
-            asyncConfig = os.path.join(FileSystem.getDirectory(FileSystem.CLIENT_CONFIG),
-                                       (asyncConfigFileName if asyncConfigFileName is not None else "AsyncConfig.xml"))
-        else:
-            asyncConfig = asyncConfigPath
-        Utilities.mkdir(outIncludeDir)
+    # def generateConfig(self, asyncConfigPath=None, asyncConfigFileName=None):
+    #     outIncludeDir = os.path.join(FileSystem.getDirectory(FileSystem.OUT_ROOT),
+    #                                  "include")
+    #     projectLogDir = FileSystem.getDirectory(FileSystem.LOG_DIR, self._config, self._project_name)
+    #     asyncConfig = None
+    #     if asyncConfigPath is None:
+    #         asyncConfig = os.path.join(FileSystem.getDirectory(FileSystem.CLIENT_CONFIG),
+    #                                    (asyncConfigFileName if asyncConfigFileName is not None else "AsyncConfig.xml"))
+    #     else:
+    #         asyncConfig = asyncConfigPath
+    #     Utilities.mkdir(outIncludeDir)
 
-        configArgs = []
+    #     configArgs = []
 
-        configArgs.append(['std::string', 'LOGGING_ROOT', 'dir', projectLogDir.replace("\\", "/")])
-        if "Robos" in self._project_name:
-            configArgs.append(['std::string', 'ASYNC_CONFIG_PATH', 'file', asyncConfig.replace("\\", "/")])
+    #     configArgs.append(['std::string', 'LOGGING_ROOT', 'dir', projectLogDir.replace("\\", "/")])
+    #     if "Robos" in self._project_name:
+    #         configArgs.append(['std::string', 'ASYNC_CONFIG_PATH', 'file', asyncConfig.replace("\\", "/")])
 
-        (formattedConfigArgsHeader, formattedConfigArgsSrc) = self.checkConfigArgsAndFormat("\t", configArgs)
+    #     (formattedConfigArgsHeader, formattedConfigArgsSrc) = self.checkConfigArgsAndFormat("\t", configArgs)
 
-        if os.path.exists(projectLogDir):
-            Utilities.rmTree(projectLogDir)
-        Utilities.mkdir(projectLogDir)
-        projNameUpper = self._project_name.upper()
-        with open(os.path.join(outIncludeDir, self._project_name + "Config.hpp"), 'w') as file:
-            file.write("#pragma once\n"
-                       "#ifndef " + projNameUpper + "_CONFIG_" + projNameUpper + "CONFIG_HPP\n"
-                       "#define " + projNameUpper + "_CONFIG_" + projNameUpper + "CONFIG_HPP\n\n"
-                       "// SYSTEM INCLUDES\n"
-                       "#include <string>\n\n"
-                       "// C++ PROJECT INCLUDES\n\n"
-                       "namespace " + self._project_name + "\n"
-                       "{\n"
-                       "namespace Config\n"
-                       "{\n\n" +
-                       formattedConfigArgsHeader +
-                       "} // end of namespace Config\n"
-                       "} // end of namespace " + self._project_name + "\n"
-                       "#endif // end of " + projNameUpper + "_CONFIG_" + projNameUpper + "CONFIG_HPP\n")
-        with open(os.path.join(outIncludeDir, self._project_name + "Config.cpp"), 'w') as file:
-            file.write("// SYSTEM INCLUDES\n\n"
-                       "// C++ PROJECT INCLUDES\n"
-                       "#include \"" + self._project_name + "Config.hpp\"\n\n"
-                       "namespace " + self._project_name + "\n"
-                       "{\n"
-                       "namespace Config\n"
-                       "{\n\n" +
-                       formattedConfigArgsSrc +
-                       "} // end of namespace Config\n"
-                       "} // end of namespace " + self._project_name + "\n")
+    #     if os.path.exists(projectLogDir):
+    #         Utilities.rmTree(projectLogDir)
+    #     Utilities.mkdir(projectLogDir)
+    #     projNameUpper = self._project_name.upper()
+    #     with open(os.path.join(outIncludeDir, self._project_name + "Config.hpp"), 'w') as file:
+    #         file.write("#pragma once\n"
+    #                    "#ifndef " + projNameUpper + "_CONFIG_" + projNameUpper + "CONFIG_HPP\n"
+    #                    "#define " + projNameUpper + "_CONFIG_" + projNameUpper + "CONFIG_HPP\n\n"
+    #                    "// SYSTEM INCLUDES\n"
+    #                    "#include <string>\n\n"
+    #                    "// C++ PROJECT INCLUDES\n\n"
+    #                    "namespace " + self._project_name + "\n"
+    #                    "{\n"
+    #                    "namespace Config\n"
+    #                    "{\n\n" +
+    #                    formattedConfigArgsHeader +
+    #                    "} // end of namespace Config\n"
+    #                    "} // end of namespace " + self._project_name + "\n"
+    #                    "#endif // end of " + projNameUpper + "_CONFIG_" + projNameUpper + "CONFIG_HPP\n")
+    #     with open(os.path.join(outIncludeDir, self._project_name + "Config.cpp"), 'w') as file:
+    #         file.write("// SYSTEM INCLUDES\n\n"
+    #                    "// C++ PROJECT INCLUDES\n"
+    #                    "#include \"" + self._project_name + "Config.hpp\"\n\n"
+    #                    "namespace " + self._project_name + "\n"
+    #                    "{\n"
+    #                    "namespace Config\n"
+    #                    "{\n\n" +
+    #                    formattedConfigArgsSrc +
+    #                    "} // end of namespace Config\n"
+    #                    "} // end of namespace " + self._project_name + "\n")
 
-    def preBuild(self, asyncConfigPath=None, asyncConfigFileName=None):
-        self.setupWorkspace()
-        self.generateProjectVersion()
-        self.generateConfig(asyncConfigPath, asyncConfigFileName)
+    # def customSetupWorkspace(self):
+    #     print("Setting up workspaces for project [%s]" % self._project_name)
+    #     self.cleanBuildWorkspace()
+    #     Utilities.mkdir(FileSystem.getDirectory(FileSystem.WORKING, self._config, self._project_name))
+    #     self.loadDependencies(self.parseDependencyFile())
+
+    # def customPreBuild(self, asyncConfigPath=None, asyncConfigFileName=None):
+    #     self.customSetupWorkspace()
+    #     self.generateProjectVersion()
+    #     self.generateConfig(asyncConfigPath, asyncConfigFileName)
 
     def help(self):
         super(LocalBuild, self).help()
@@ -127,7 +169,4 @@ if __name__ == "__main__":
     print(customCommands)
 
     localBuild = LocalBuild()
-    if "help" in customCommands:
-        localBuild.help()
-    else:
-        localBuild.run(customCommands)
+    localBuild.run(customCommands)
