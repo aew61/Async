834489ab98420f78292dbd544ce2f78e13f8030c
Andrew Wood <aew61@case.edu>
2016-07-28 15:40:35 -0400

    see if projects can link

--- cpp/src/Async/AddInts.cpp
+++ cpp/src/Async/AddInts.cpp
@@ -1,5 +1,5 @@
 // SYSTEM INCLUDES
-
+#include <Utilities/AddInts.h>
 
 // C++ PROJECT INCLUDES
 #include "Async/AddInts.h"
@@ -9,7 +9,7 @@ namespace Async
 
     int AddInts(int a, int b)
     {
-        return a + b;
+        return Utilities::AddInts(a, b);
     }
 
 } // end of namespace Async
