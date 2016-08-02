83d3a316926b1b8c04f9bb7fd6ed4393ae68cbe6
Andrew Wood <aew61@case.edu>
2016-08-02 12:38:57 -0400

    changing cmake imports

--- workspace/Async/cmake/modules/FindAsync.cmake
+++ workspace/Async/cmake/modules/FindAsync.cmake
@@ -12,7 +12,7 @@ if( ${CMAKE_SYSTEM_NAME} MATCHES "Windows" )
             ${CMAKE_INSTALL_PREFIX}/bin
             ${CMAKE_PREFIX_PATH}/bin
     )
-    find_library( ASYNC_LIB Async
+    find_library( ASYNC_LIB_IMPL Async
         HINTS
             ${CMAKE_INSTALL_PREFIX}/lib
             ${CMAKE_PREFIX_PATH}/lib
@@ -25,9 +25,24 @@ else()
     )
 endif()
 
-if( NOT ASYNC_INCLUDES OR NOT ASYNC_SHARED_LIB )
-    set( ASYNC_FOUND FALSE )
+# make library target
+if( NOT TARGET ASYNC_LIB )
+    add_library( ASYNC_LIB SHARED IMPORTED )
+
+    # will set .so for unix systems and .dll for windows
+    set_property( TARGET ASYNC_LIB PROPERTY
+        IMPORTED_LOCATION ${ASYNC_SHARED_LIB} )
+
+    # need to link to .lib files for windows
+    if( ${CMAKE_SYSTEM_NAME} MATCHES "Windows" )
+        set_property( TARGET ASYNC_LIB PROPERTY
+            IMPORTED_IMPLIB ${ASYNC_LIB_IMPL} )
+    endif( ${CMAKE_SYSTEM_NAME} MATCHES "Windows" )
+endif()
+
+if( ASYNC_INCLUDES AND ASYNC_LIB )
+    set( ASYNC_FOUND TRUE )
 else()
     # set( ASYNC_SHARED_LIB ${ASYNC_SHARED_LIB}/Async.dll )
-    set( ASYNC_FOUND TRUE )
+    set( ASYNC_FOUND FALSE )
 endif()
